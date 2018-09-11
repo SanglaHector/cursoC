a249104538512577a8e764026e7464d2e533be7a
alumno <alumno@utnfra.com.ar>
2018-09-11 11:33:24 -0300

    matrices

--- ejemplo clase 6/problema/main.c
+++ ejemplo clase 6/problema/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+# define T 3
 int main()
 {
    /* produco:
@@ -21,7 +22,33 @@ int main()
         int legajos[10];
         char nombres[10][21];   //nombres[10] solo puedo guardar nombres de un solo tipo, o un  solo nombre. Tengo que definir una matriz!!
         int nota[10];
-        float alturas [10];
+        float altura [10];
+        int i;
+
+        for ( i = 0 ; i < T ; i++)
+        {//En el mismo for pido todos los datos
+            printf("Ingrese legajo: ");
+            scanf("%d",&legajos[i]);
+            printf("Ingrese el nombre: ");
+            fflush(stdin);//En el siguiente paso tengo que ser riguroso, si pongo gets(nombres) guardo todo la matriz, entonces tengo que aclarar en que fila
+            gets(nombres[i]);
+            printf("Ingrese la nota : ");
+            scanf("%d",&nota[i]);
+            printf("Ingrese altura: ");
+            scanf("%f",&altura[i]);
+        }
+        //Muestro los datos, hago otro for para recorrer los arrays
+
+        printf("Legajo Nombre  Nota  Altura\n");
+        for(i=0;i< T;i++)
+        {
+            printf("%d  %s  %d  %f\n", legajos[i] , nombres[i],nota[i],altura[i]);
+        }fflush("stdio");
 
     return 0;
 }
+// para que quede acomodado
+// printf("%4d %20s %2d %4f \n","legajos","nombres"," nota","altura");
+//      for(--------)
+//          {printf("%4d %20s %2d %4f \n", legajos[i] , nombres[i] , nota[i] , altura[i]);
+//
