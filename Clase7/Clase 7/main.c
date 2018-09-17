f1e70cda5ed90bfa7c715212a879d09cf510691c
alumno <alumno>
2018-09-17 12:10:37 -0300

    clase 7

--- Clase7/Clase 7/main.c
+++ Clase7/Clase 7/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "alumno.h"
+typedef struct
+{
+    int legajo;
+    char nombre [50];
+    float altura;
+    int nota;
+}eAlumno;
 
+void cargarListaDeAlumnos(eAlumno [],  int);
+void mostrarListaDeAlumnos(eAlumno[], int);
 int main()
 {
+    eAlumno listaDeAlumnos[5];
 
+
+   cargarListaDeAlumnos(listaDeAlumnos, 5);
+   mostrarListaDeAlumnos(listaDeAlumnos,5);
     return 0;
 }
+
+/*
+void ordenarPorNombre ( eAlumno listaDeAlumnos[], int tam)
+{
+    int i;
+    for ( i = 0 ; i < tam ; i++)
+    {
+
+    }
+}
+*/
