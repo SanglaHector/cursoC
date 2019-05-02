06e31a72114bc59ca2919f84325ae22d4315d4af
SanglaHector <40794895+SanglaHector@users.noreply.github.com>
2019-05-02 15:18:29 -0300

    Add files via upload

---
+++ validaciones.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
+/**
+**/
+int validarInt(int num,int min,int max);
+//*****************************************************
+/**
+**/
+int validarFloat(float num, float min, float max);
+//****************************************************
+/**
+**/
+int validarNombre(char nombre[]);
+/**
+**/
