47e9bac60e0d720b12de18ae8825e90703a16b96
alumno <alumno>
2019-05-17 19:47:08 -0300

    17/05

--- ClasePunteros/ClasePunteros/main.c
+++ ClasePunteros/ClasePunteros/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//*******************************
 int main()
 {
+    int x = 5;
+    int* p = &x;
 
+    modificarNumero(p);
+
+    printf("%d", *p);
     return 0;
 }
+//     &X SIGNIFICA LA UBICACION DE: X
+//     X* SIGNIFICA HACIA A DONDE APUNTA: X
+//********************************
+void  modificarNumero(int * a){
+*a = 25;
+}
+//********************************
