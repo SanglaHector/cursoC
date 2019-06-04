5fc95d4f87291ac5863d4d2ef5f7bf3832b196f2
alumno <alumno@utnfra.com.ar>
2019-06-03 22:10:54 -0300

    tp.3

--- TP3/Win_64/main.c
+++ TP3/Win_64/main.c
@@ -21,7 +21,7 @@
 
 int main()
 {
-    int option = 0;
+//    int option = 0;
     LinkedList* listaEmpleados = ll_newLinkedList();
     /*do{
         switch(option)
@@ -34,15 +34,47 @@ int main()
     }while(option != 10);
     return 0;*/
 
-
+//harcodeo 3 emnplados
     Employee* emp1 = employee_newParametros("1234","Juan","234","25000");
     Employee* emp2 = employee_newParametros("2222","Julia","100","15000");
     Employee* emp3 = employee_newParametros("1111","Juana","340","35000");
+    Employee* emp4 = employee_newParametros("5555","Sofia","250","26000");
 
+    //muestro cantidad de elementos dentro del la lista vacia
     printf("\nCantidad de elementos: %d\n",ll_len(listaEmpleados) );
     if(ll_add(listaEmpleados,emp1)== 0)
     {
         printf("\nSe ha agregado el empleado a la lista!\n");
     }
     printf("\nCantidad de elementos: %d\n",ll_len(listaEmpleados) );
+
+
+    if(ll_add(listaEmpleados,emp2)== 0)
+    {
+        printf("\nSe ha agregado el empleado a la lista!\n");
+    }
+    printf("\nCantidad de elementos: %d\n",ll_len(listaEmpleados) );
+
+
+
+    if(ll_add(listaEmpleados,emp3)== 0)
+    {
+        printf("\nSe ha agregado el empleado a la lista!\n");
+    }
+    printf("\nCantidad de elementos: %d\n",ll_len(listaEmpleados) );
+
+
+    if(ll_add(listaEmpleados,emp4)== 0)
+    {
+        printf("\nSe ha agregado el empleado a la lista!\n");
+    }
+    printf("\nCantidad de elementos: %d\n",ll_len(listaEmpleados) );
+
+
+    mostrarEmpleado( (Employee*) ll_get(listaEmpleados,1));//casteo aunqeu de igual forma funciona
+    if(ll_set(listaEmpleados,1,emp4) == 0)
+    {
+
+    }
+
 }
